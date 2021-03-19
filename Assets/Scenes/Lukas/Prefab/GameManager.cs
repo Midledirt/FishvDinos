e412caae3567a04e8bd4b1ef418ef37ad01c7543
Lukas <tsuki32@hotmail.com>
2021-03-19 18:23:53 +0100

    Grid and placement
    Programming

---
+++ Assets/Scenes/Lukas/Prefab/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject draggingObject;
+    public GameObject currentContainer;
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void PlaceObject()
+    {
+       if (draggingObject != null && currentContainer != null)
+       {
+            Instantiate(draggingObject.GetComponent<ObjectDragged>().card.object_game, currentContainer.transform);
+            currentContainer.GetComponent<ObjectContainer>().isFull = true;
+       } 
+    }
+
+}
---
+++ Assets/Scenes/Lukas/Prefab/ObjectCard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+
+
+public class ObjectCard : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+    public GameObject object_drag;
+    public GameObject object_game;
+    public Canvas canvas;
+    private GameObject objectDragInstance;
+    private GameManager gameManager;
+
+
+    public void Start()
+    {
+        gameManager = GameManager.instance;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        objectDragInstance.transform.position = Input.mousePosition;
+    }
+
+
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+       objectDragInstance = Instantiate(object_drag, canvas.transform);
+       objectDragInstance.transform.position = Input.mousePosition;
+       objectDragInstance.GetComponent<ObjectDragged>().card = this;
+
+        gameManager.draggingObject = objectDragInstance;
+    }
+
+    
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        gameManager.PlaceObject();
+        gameManager.draggingObject = null;
+        Destroy(objectDragInstance);
+
+    }
+}
